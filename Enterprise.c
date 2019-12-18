ec684883bb733df926aef9249feebb11b9e1ed6e
AlonVaisgur <37458479+Romeore@users.noreply.github.com>
2019-12-19 00:48:57 +0200

    To beyond and infinity !

--- Enterprise.c
+++ Enterprise.c
@@ -1,7 +1,18 @@
 //Enterprise.c
 #pragma once
 
-#include "AlonStrings.h"
+#include <stdio.h>
+
+#define ZERO    0
+#define ONE     1
+#define TWO     2
+#define THREE   3
+#define EIGHT   8
+#define FIFTEEN 15
+#define BOOLEAN unsigned short
+#define TRUE    1
+#define FALSE   0
+#define ABS(x)  (x > 0) ? (x) : (-x)
 
 typedef struct Creature
 {
@@ -10,6 +21,116 @@ typedef struct Creature
 	char bornPlanet[FIFTEEN];
 	int  age;
 } Creature;
+
+//----------------------------------------------------------------------
+//                         Binary Search File
+//                         ------------------
+//
+// General      : This function moves the file pointer to the creature
+//                code with binary search.
+//
+// Parameters   : 
+//			ptrStarShipFile - A pointer to the file (FILE**)		
+//          creatureCode    - The code to search the creature.
+//
+// Return Value : None. 
+//
+//----------------------------------------------------------------------
+
+void binarySearchFile (FILE** ptrStarShipFile, int creatureCode)
+{
+	Creature tempCreature;
+	BOOLEAN foundCode = FALSE;
+	int limitBottom = ZERO;
+	int limitTop;
+	int mid;
+
+	// Find the amount of creatures.
+	fseek(*ptrStarShipFile, 0, SEEK_END);
+	limitTop = (ftell(*ptrStarShipFile) / sizeof(Creature)) - ONE;
+
+	// Find the place of the code.
+	while (limitTop >= limitBottom && (!foundCode))
+	{
+		mid = (limitTop + limitBottom) / TWO;
+		fseek(*ptrStarShipFile, sizeof(Creature) * mid, SEEK_SET);
+		fread(&tempCreature, sizeof(Creature), ONE, *ptrStarShipFile);
+
+		if (tempCreature.creatureCode == creatureCode)
+		{
+			foundCode = TRUE;
+		}
+
+		(tempCreature.creatureCode > creatureCode) ?
+			(limitTop = mid - ONE) : (limitBottom = mid + ONE);
+	}
+
+	// Move one place back to read the place again.
+	fseek(*ptrStarShipFile, -(long)(sizeof(Creature)), SEEK_CUR);
+	return;
+}
+
+//----------------------------------------------------------------------
+//                         Binary Add To File
+//                         ------------------
+//
+// General      : This function moves the file pointer to the creature
+//                code with binary search to add to the file.
+//
+// Parameters   : 
+//			ptrStarShipFile - A pointer to the file (FILE**)		
+//          creatureCode    - The code to search the creature.
+//
+// Return Value : None. 
+//
+//----------------------------------------------------------------------
+
+void binaryAddToFile(FILE** ptrStarShipFile, int creatureCode)
+{
+	Creature tempCreature;
+	BOOLEAN foundCode = FALSE;
+	int limitBottom = ZERO;
+	int limitTop;
+	int mid;
+	int size;
+
+	// Find the amount of creatures.
+	fseek(*ptrStarShipFile, 0, SEEK_END);
+	size = (ftell(*ptrStarShipFile)) / sizeof(Creature) - ONE;
+	limitTop = size;
+	
+	// Find the place of the closest code to the input code.
+	while (limitTop >= limitBottom)
+	{
+		mid = (limitTop + limitBottom) / TWO;
+		fseek(*ptrStarShipFile, sizeof(Creature) * mid, SEEK_SET);
+		fread(&tempCreature, sizeof(Creature), ONE, *ptrStarShipFile);
+
+		(tempCreature.creatureCode > creatureCode) ?
+			(limitTop = mid - ONE) : (limitBottom = mid + ONE);
+	}
+	// If the limit top is minus, make it positive.
+	ABS(limitTop);
+	// Check if the currect creature code is bigger than the new one
+	fseek(*ptrStarShipFile, sizeof(Creature) * limitTop, SEEK_SET);
+	fread(&tempCreature, sizeof(Creature), ONE, *ptrStarShipFile);
+	// If its bigger, add it before the currect creature code.
+	(tempCreature.creatureCode > creatureCode) ? limitTop-- : limitTop;
+
+	// Move the creatures that above the creature code one place.
+	for (; limitTop < size; size--)
+	{
+		fseek(*ptrStarShipFile, sizeof(Creature) * (size), SEEK_SET);
+		fread(&tempCreature, sizeof(Creature), ONE, *ptrStarShipFile);
+		fseek(*ptrStarShipFile, ZERO, SEEK_CUR);
+		fwrite(&tempCreature, sizeof(Creature), ONE, *ptrStarShipFile);
+	}
+
+	// Place the file pointer to the writeable place.
+	fseek(*ptrStarShipFile, sizeof(Creature) * (limitTop + ONE), SEEK_SET);
+
+	return;
+}
 
 //----------------------------------------------------------------------
 //                           Print Creature
@@ -26,6 +147,7 @@ typedef struct Creature
 
 void printCreature(Creature* ptrCreature)
 {
+	// Print the creature parameters.
 	printf("\nCreature code: %d\n", ptrCreature->creatureCode);
 	printf("\nCreature name: %s\n", ptrCreature->creatureName);
 	printf("\nBorn planet: %s\n", ptrCreature->bornPlanet);
@@ -48,45 +170,18 @@ void printCreature(Creature* ptrCreature)
 
 void printAllCreatures()
 {
+	// Open file
 	FILE* starShipFile = fopen("starship.dat", "rb");
 	Creature tempCreature;
 
+	// Print each creature in the file
 	while (fread(&tempCreature, sizeof(Creature), ONE, starShipFile))
 	{
 		printCreature(&tempCreature);
 	}
 	
+	// Close file
 	fclose(starShipFile);
-	return;
-}
-
-//----------------------------------------------------------------------
-//                         Seek To Creature By Code
-//                         ------------------------
-//
-// General      : This function moves the file pointer to the creature
-//                code.
-//
-// Parameters   : 
-//			ptrStarShipFile - A pointer to the file (FILE**)		
-//          creatureCode    - The code to search the creature.
-//
-// Return Value : None. 
-//
-//----------------------------------------------------------------------
-
-void seekToCreatureByCode(FILE** ptrStarShipFile, int creatureCode)
-{
-	Creature tempCreature;
-	BOOLEAN foundCode = FALSE;
-
-	while (fread(&tempCreature, sizeof(Creature), ONE, *ptrStarShipFile) && !foundCode)
-	{
-		(tempCreature.creatureCode == creatureCode) ?
-			(foundCode = TRUE) : (foundCode);
-	}
-
-	fseek(*ptrStarShipFile, (ftell(*ptrStarShipFile)) - sizeof(Creature), SEEK_SET);
 
 	return;
 }
@@ -105,19 +200,18 @@ void seekToCreatureByCode(FILE** ptrStarShipFile, int creatureCode)
 //
 //----------------------------------------------------------------------
 
-BOOLEAN isCreatureCodeExist(int creatureCode)
+BOOLEAN isCreatureCodeExist(FILE* starShipFile, int creatureCode)
 {
-	FILE* starShipFile = fopen("starship.dat", "rb");
+	BOOLEAN foundCode;
 	Creature tempCreature;
-	BOOLEAN foundCode = FALSE;
 
-	while (fread(&tempCreature, sizeof(Creature), ONE, starShipFile) && !foundCode)
-	{
-		(tempCreature.creatureCode == creatureCode) ?
-			(foundCode = TRUE) : (foundCode);
-	}
+	// Find the place to read the creature that matches the code.
+	binarySearchFile(&starShipFile, creatureCode);
+	fread(&tempCreature, sizeof(Creature), ONE, starShipFile);
 
-	fclose(starShipFile);
+	// Check if the read is success.
+	(tempCreature.creatureCode == creatureCode) ?
+		(foundCode = TRUE) : (foundCode = FALSE);
 
 	return (foundCode);
 }
@@ -137,6 +231,7 @@ BOOLEAN isCreatureCodeExist(int creatureCode)
 
 void scanACreature(Creature* inputCreature)
 {
+	// Scan the creature parameters.
 	printf("\nPlease write the creature name\n");
 	scanf("%s", inputCreature->creatureName);
 
@@ -161,29 +256,32 @@ void scanACreature(Creature* inputCreature)
 //
 //----------------------------------------------------------------------
 
-void addCreature()
+void addACreature()
 {
-	FILE* starShipFile = fopen("starship.dat", "ab");
-
-	BOOLEAN foundCode;
+	// Open file
+	FILE* starShipFile = fopen("starship.dat", "r+b");
 	Creature addedCreature;
 	
+	// Get the creature code.
 	printf("\nPlease write the creature code\n");
 	scanf("%d", &addedCreature.creatureCode);
 
-	foundCode = isCreatureCodeExist(addedCreature.creatureCode);
-
-	while (foundCode)
+	// Checks if the code already exists, and if it does, ask again.
+	while (isCreatureCodeExist(starShipFile, addedCreature.creatureCode))
 	{
 		printf("\nThe creature code already exists, please write another code.\n");
 		scanf("%d", &addedCreature.creatureCode);
-		foundCode = isCreatureCodeExist(addedCreature.creatureCode);
 	}
 
+	// Scan the added creature.
 	scanACreature(&addedCreature);
-	fwrite(&addedCreature, sizeof(Creature), 1, starShipFile);
-
+	// Find its place on the file and write it.
+	binaryAddToFile(&starShipFile, addedCreature.creatureCode);
+	fwrite(&addedCreature, sizeof(Creature), ONE, starShipFile);
+	
+	// Close file
 	fclose(starShipFile);
+
 	return;
 }
 
@@ -201,29 +299,31 @@ void addCreature()
 
 void updateACreature()
 {
+	// Open file
 	FILE* starShipFile = fopen("starship.dat", "r+b");
-	BOOLEAN success = FALSE;
 	Creature updatedCreature;
 
+	// Get the creature code.
 	printf("\nWrite the creature code of a creature you want to change\n");
 	scanf("%d", &(updatedCreature.creatureCode));
-	success = isCreatureCodeExist(updatedCreature.creatureCode);
 
-	while (!success)
+	while (!isCreatureCodeExist(starShipFile, updatedCreature.creatureCode))
 	{
 		printf("\nThe creature code is not found, please write a valid code.\n");
 		scanf("%d", &(updatedCreature.creatureCode));
-		success = isCreatureCodeExist(updatedCreature.creatureCode);
 	}
 
+	// Scan the updated creature.
 	scanACreature(&updatedCreature);
-	seekToCreatureByCode(&starShipFile, updatedCreature.creatureCode);
-	fwrite(&updatedCreature, sizeof(Creature), 1, starShipFile);
+	// Find its place on the file and write it.
+	binarySearchFile(&starShipFile, updatedCreature.creatureCode);
+	fwrite(&updatedCreature, sizeof(Creature), ONE, starShipFile);
+
+	// Close file
 	fclose(starShipFile);
 
 	return;
 }
-	
 
 //-----------------------------------------------------------------------------
 //                                   Enterprise
@@ -251,7 +351,7 @@ void main(void)
 	int option;
 
 	// Code section
-	
+
 	while (!isFinished)
 	{
 		// Show options
@@ -268,7 +368,7 @@ void main(void)
 		{
 		case ONE: printAllCreatures();
 			break;
-		case TWO: addCreature();
+		case TWO: addACreature();
 			break;
 		case THREE: updateACreature();
 			break;
@@ -277,7 +377,7 @@ void main(void)
 			break;
 		}
 	}
-
-	// Print end
+	
+	// End print
 	printf("\nThank you for using !\n");
 }
